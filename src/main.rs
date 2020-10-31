99f29c06f05036f228709b9d5e067a5dbfad86ef
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-31 17:59:55 -0300

    mount app

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #![feature(decl_macro)]
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use rocket::response::content::Json;
 
 #[get("/hello")]
 fn hello() -> Json<&'static str> {
-    Json("{
+  Json("{
     'status': 'success',
     'message': 'Hello API!'
   }")
 }
 
 fn main() {
-    println!("Hello, world!");
+  rocket::ignite()
+    .mount("/api", routes![hello])
+    .launch();
 }

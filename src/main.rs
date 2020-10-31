67371e32a04218660cc35c78a54df1ee3189beed
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-31 18:12:06 -0300

    add POST book route

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::request::Form;
 use rocket::response::content::Json;
+
+#[derive(FromForm, Debug)]
+struct Book {
+  title: String,
+  author: String,
+  isbn: String,
+}
 
 #[get("/hello")]
 fn hello() -> Json<&'static str> {
@@ -10,6 +18,14 @@ fn hello() -> Json<&'static str> {
     'status': 'success',
     'message': 'Hello API!'
   }")
+}
+
+#[post("/book", data = "<book_form>")]
+fn new_book(book_form: Form<Book>) -> String {
+  let book: Book = book_form.into_inner();
+  let mut dummy_db: Vec<Book> = Vec::new();
+  dummy_db.push(book);
+  format!("Book added successfully: {:?}", dummy_db)
 }
 
 fn main() {
